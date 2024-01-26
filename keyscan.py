8d189c0939b1e3f6fc8a04c428ff2c6b5adbad52
Jitin25610 <155473463+Jitin25610@users.noreply.github.com>
2024-01-26 14:29:45 +0530

    Add files via upload

---
+++ keyscan.py
@@ -0,0 +1,12 @@
+import pyautogui
+import time
+
+# Pause for 15 seconds
+time.sleep(15)
+
+# Press the right arrow key
+pyautogui.press('right')
+
+# Press the enter key
+pyautogui.press('enter')
+
